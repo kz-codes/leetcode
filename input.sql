14b0db55787ec4cfd85869023e530812bc17cd23
kz-codes <aman642.g@gmail.com>
2026-01-03 15:24:11 +0530

    db1

--- input.sql
+++ input.sql
@@ -1,6 +1,10 @@
-Create table If Not Exists Employee (id int, name varchar(255), salary int, managerId int);
-Truncate table Employee;
-insert into Employee (id, name, salary, managerId) values ('1', 'Joe', '70000', '3');
-insert into Employee (id, name, salary, managerId) values ('2', 'Henry', '80000', '4');
-insert into Employee (id, name, salary, managerId) values ('3', 'Sam', '60000', NULL);
-insert into Employee (id, name, salary, managerId) values ('4', 'Max', '90000', NULL);
+Create table If Not Exists Customers (id int, name varchar(255));
+Create table If Not Exists Orders (id int, customerId int);
+Truncate table Customers;
+insert into Customers (id, name) values ('1', 'Joe');
+insert into Customers (id, name) values ('2', 'Henry');
+insert into Customers (id, name) values ('3', 'Sam');
+insert into Customers (id, name) values ('4', 'Max');
+Truncate table Orders;
+insert into Orders (id, customerId) values ('1', '3');
+insert into Orders (id, customerId) values ('2', '1');
